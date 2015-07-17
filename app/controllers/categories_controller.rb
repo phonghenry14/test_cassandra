d65d70e21c62eace81b30d8ec282cc76667ac98e
phonghenry14 <tran.duc.phong@framgia.com>
2015-07-17 14:38:56 +0700

    edit, delete message and category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,12 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, except: [:index, :new, :create]
+
   def index
     @categories = Category.all
   end
 
   def show
-    @category = Category.find params[:id]
-    @messages = Message.where(:category_id, @category.id)
+    @messages = Message[@category.id]
   end
 
   def new
@@ -23,8 +24,34 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = "Update category successfully"
+      redirect_to category_path @category
+    else
+      flash[:danger] = "Update category failed"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      flash[:success] = "Delete category successfully"
+    else
+      flash[:danger] = "Delete category failed"
+    end
+    redirect_to categories_path
+  end
+
   private
   def category_params
     params.require(:category).permit :name, :description, :id
+  end
+
+  def set_category
+    @category = Category.find_by_id params[:id]
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+
   def index
     if params[:query].present?
       @messages = Message.search_with_elastic params[:query]
@@ -22,8 +24,34 @@ class MessagesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @message.update_attributes message_params
+      flash[:success] = "Update message successfully"
+      redirect_to message_path @message
+    else
+      flash[:danger] = "Update message failed"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @message.destroy
+      flash[:success] = "Delete message successfully"
+    else
+      flash[:danger] = "Delete message failed"
+    end
+    redirect_to messages_path
+  end
+
   private
   def message_params
-    params.require(:message).permit :content, :category_id, :id
+    params.require(:message).permit :content, :category_id
+  end
+
+  def set_message
+    @message = Message[params[:category_id]][params[:id]]
   end
 end

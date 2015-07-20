d0a2377985e32f70f3b5710bd85574babed79f25
phonghenry14 <tran.duc.phong@framgia.com>
2015-07-20 16:38:04 +0700

    using redis to store page views of each message

---
+++ lib/myapp/redis.rb
@@ -0,0 +1,7 @@
+class Myapp::Redis < Redis
+  REDIS_CONFIG = YAML.load_file(::Rails.root.join "config/database.yml")[::Rails.env]
+
+  def initialize db = 0
+    super host: REDIS_CONFIG["redis"]["host"], db: db
+  end
+end
